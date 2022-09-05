fc20c2fc34fc6ccdd63258f2547128a75adb17ef
Matheus Fellype <matheusftrab@gmail.com>
2022-09-05 18:34:45 -0400

    teste

--- pages/stack/methods/methods.jsx
+++ pages/stack/methods/methods.jsx
@@ -1,6 +1,6 @@
 
 export const methods = [
-  { nameMethod: "Criar Pilha", link: "stack/methods/createstack" },
+  { nameMethod: "Criar Pilha", link: "stack/methods/CreateStack" },
   { nameMethod: "Liberar Pilha", link: "stack/methods/freestack" },
   { nameMethod: "Pilha Vazia", link: "stack/methods/isempty" },
   { nameMethod: "Pilha Cheia", link: "stack/methods/isfull" },
